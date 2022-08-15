d6e269c7348ce2a011bcd839745e2b7941944c5b
YehorVoitenko <egorka.voitenko@gmail.com>
2022-08-15 16:34:39 +0300

    Added city location

--- main_project/table/user.py
+++ main_project/table/user.py
@@ -1,4 +1,4 @@
-from base.base import Base
+from main_project.base.base import Base
 from sqlalchemy import Column, Integer, String
 
 
@@ -8,9 +8,13 @@ class User(Base):
     name = Column(String)
     surname = Column(String)
     telephone_number = Column(Integer)
+    location_city = Column(String)
 
     def __repr__(self):
-        return f"<User{self.id}\n" \
+        return f"<User {self.id}\n" \
                f"Name: '{self.name}'\n" \
                f"Surname: '{self.surname}'\n" \
-               f"Telephone number: {self.telephone_number}'>"
+               f"Telephone number: '{self.telephone_number}'\n" \
+               f"City: '{self.location_city}'>"
+
+
