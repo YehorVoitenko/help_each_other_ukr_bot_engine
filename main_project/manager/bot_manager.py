e0190ef705d68b0790fbf7dbde5579a9da15eb39
YehorVoitenko <egorka.voitenko@gmail.com>
2022-08-16 11:33:56 +0300

    Refactored

--- main_project/manager/bot_manager.py
+++ main_project/manager/bot_manager.py
@@ -9,35 +9,56 @@ Base.metadata.create_all(engine)
 
 interface_phrase = get_translation()
 
-choose_your_report = input(interface_phrase.ask_user_what_he_looking_for)
+# def check_value(*args):
+#     for value in args:
+#         if isinstance(value, str):
+#             result = True
+#         else:
+#             print(f'Enter only worlds in \'{value}\'')
+#             result = False
+#             return result
+#     return result
+
+
+def choose_your_report():
+    report_type = input(interface_phrase.ask_user_what_he_looking_for)
+    if report_type == interface_phrase.need or report_type == interface_phrase.give:
+        return report_type
+    else:
+        return f'{interface_phrase.syntax_error}'
 
 
 def get_info_about_helper():
-    name: str = input(interface_phrase.get_name)
+    name = input(interface_phrase.get_name)
     surname: str = input(interface_phrase.get_surname)
-    telephone_number: str = input(interface_phrase.get_telephone_number)
+    telephone_number: int = int(input(interface_phrase.get_telephone_number))
     location_city: str = input(interface_phrase.get_users_city)
-
+    help_description: str = input(interface_phrase.write_help_description)
+    # if check_value(name, surname, telephone_number, location_city):
     user_info = Helpers(
         name=name,
         surname=surname,
         location_city=location_city,
         telephone_number=telephone_number,
+        help_description=help_description,
     )
     return save_user(user_info)
 
 
-#  Add handler 'start'  #  TODO: ADD TWO BOTS FOR DIVIDE SERVICES (ONE FOR SEARCH, ONE FOR ADD)
-if choose_your_report == interface_phrase.need:  # TODO: ADD BUTTON
+def interact_with_needy():  # TODO: ADD BUTTON
+    #  Add handler 'start'  #  TODO: ADD TWO BOTS FOR DIVIDE SERVICES (ONE FOR SEARCH, ONE FOR ADD)
     needed_city = input(interface_phrase.report_for_find_city)
-    needy = get_user_info_by_city(needed_city)
-    print(needy)
+    print(get_user_info_by_city(needed_city))
 
-elif choose_your_report == interface_phrase.give:  # TODO: ADD BUTTON
+
+def interact_with_helper():  # TODO: ADD BUTTON
     print(interface_phrase.enter_self_info)
     get_info_about_helper()
     print(interface_phrase.thanks_for_help, '\n')
     user = get_user_info_all()
     final_user = user.pop()
     print(final_user)
+
+
+
 
