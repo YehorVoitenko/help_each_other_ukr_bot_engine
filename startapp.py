0d50ec85424196735e00c85cb3ce0b92ab6647ea
YehorVoitenko <egorka.voitenko@gmail.com>
2022-08-16 11:33:43 +0300

    Add startapp.py block to start project

---
+++ startapp.py
@@ -0,0 +1,16 @@
+from main_project.manager.bot_manager import choose_your_report, interact_with_needy, interact_with_helper
+from main_project.utils.file_utils import get_translation
+
+interface_phrase = get_translation()
+
+interact_type = choose_your_report()
+
+
+if interact_type == interface_phrase.give:
+    interact_with_helper()
+elif interact_type == interface_phrase.need:
+    interact_with_needy()
+else:
+    print(interface_phrase.syntax_error)
+
+
