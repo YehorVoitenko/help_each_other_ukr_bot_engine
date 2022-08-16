e39c6c125af718ad5fef76f9b831ebee6b4c03ed
YehorVoitenko <egorka.voitenko@gmail.com>
2022-08-16 11:33:22 +0300

    Add new line 'help description'

--- main_project/models/table/helpers.py
+++ main_project/models/table/helpers.py
@@ -4,17 +4,20 @@ from sqlalchemy import Column, Integer, String
 
 class Helpers(Base):
     __tablename__ = 'helpers'
+
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    surname = Column(String)
-    telephone_number = Column(Integer)
-    location_city = Column(String)
+    name = Column(String(20))
+    surname = Column(String(20))
+    telephone_number = Column(String(12))
+    location_city = Column(String(20))
+    help_description = Column(String(250))
 
     def __repr__(self):
         return f"<User {self.id}\n" \
                f"Name: '{self.name}'\n" \
                f"Surname: '{self.surname}'\n" \
                f"Telephone number: '{self.telephone_number}'\n" \
-               f"City: '{self.location_city}'>"
+               f"City: '{self.location_city}'\n" \
+               f"Description {self.help_description}>"
 
 
--- main_project/utils/string_utils.py
+++ main_project/utils/string_utils.py
@@ -1,7 +1,8 @@
 class Translation:
     def __init__(self, get_name, get_surname, get_telephone_number,
                  get_users_city, report_for_find_city, ask_user_what_he_looking_for,
-                 enter_self_info, give, need, thanks_for_help):
+                 enter_self_info, give, need, thanks_for_help, write_help_description,
+                 syntax_error):
         self.get_name = get_name
         self.get_surname = get_surname
         self.get_telephone_number = get_telephone_number
@@ -10,5 +11,7 @@ class Translation:
         self.ask_user_what_he_looking_for = ask_user_what_he_looking_for
         self.enter_self_info = enter_self_info
         self.thanks_for_help = thanks_for_help
+        self.write_help_description = write_help_description
         self.give = give
         self.need = need
+        self.syntax_error = syntax_error
